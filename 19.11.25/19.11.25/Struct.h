39c8783730ad08b4ef193a48f361867f1f82b1c6
명균 양 <krymk1212@naver.com>
2019-12-04 16:46:24 +0900

    19.12.04

--- 19.11.25/19.11.25/Struct.h
+++ 19.11.25/19.11.25/Struct.h
@@ -1,2 +1,15 @@
 #pragma once
 
+struct Vector3
+{
+	float x, y;
+};
+
+struct Object
+{
+	char* pTexture;
+	Vector3 Position;
+	Vector3 Scale;
+
+	int Rotate;
+};
--- 19.12.04/19.12.04/Headers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <Windows.h>
-#include <time.h>
-
-#include "Struct.h"
-#include "Enum.h"
